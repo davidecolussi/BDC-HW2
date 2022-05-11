b233585b10c31c6cecb6fbd9389815bd3255bc69
Davide <davide.colussi.99@gmail.com>
2022-05-11 11:09:49 +0200

    added comments

--- G074HW2.java
+++ G074HW2.java
@@ -35,20 +35,37 @@ public class G074HW2 {
         return result;
     }
 
+    /**
+     * This method implements the weighted variant of KcenterOUT algorithm
+     * @param P the set of points
+     * @param W the set of weights
+     * @param k the number of centers
+     * @param z the number of outliers
+     * @param alpha coefficient used by the algorithm
+     * @return a solution of the K-center with z outliers problem
+     */
     public  static ArrayList<Vector> SeqWeightedOutliers(ArrayList<Vector> P, ArrayList<Long> W, int k, int z, int alpha) {
         return null;
     }
 
+    /**
+     * This method computes the value of the objective function of the K-center with z outliers problem
+     * @param P the set of points
+     * @param S the solution set
+     * @param z the number of outliers
+     * @return the value of the objective function
+     */
     public static double ComputeObjective(ArrayList<Vector> P, ArrayList<Vector> S, int z) {
         return 0;
     }
 
     public static void main(String[] args) throws IOException {
-
+        System.out.println("Hello world");
         ArrayList<Vector> inputPoints = readVectorsSeq(args[0]); //File reading
         int k = Integer.parseInt(args[1]); //Number of centers
         int z = Integer.parseInt(args[2]); //Number of allowed outliers
 
+        //create the vector of weight (for homework 2 all 1s)
         ArrayList<Long> weights = new ArrayList<Long>();;
         for(int i = 0; i<inputPoints.size(); i++) {
             weights.add(1L);
