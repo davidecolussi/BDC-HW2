b3854de4124a20085e7bf31f1a204b66469fde0b
Davide <davide.colussi.99@gmail.com>
2022-05-12 17:47:27 +0200

    attempt for seqWeighted function 2

--- G074HW2.java
+++ G074HW2.java
@@ -36,93 +36,17 @@ public class G074HW2 {
     }
 
     /**
-     * This method implements the weighted variant of KcenterOUT algorithm
-     * @param P the set of points
-     * @param W the set of weights
-     * @param k the number of centers
-     * @param z the number of outliers
-     * @param alpha coefficient used by the algorithm
-     * @return a solution of the K-center with z outliers problem
+     * DUMMY SOLUTION FOR SEQWEIGHTOUTLIERS
+     * @param P
+     * @param W
+     * @param k
+     * @param z
+     * @param alpha
+     * @return
      */
-    public static ArrayList<Vector> SeqWeightedOutliers(ArrayList<Vector> P, ArrayList<Long> W, int k, int z, int alpha) {
-
-        double minDist = Math.sqrt(Vectors.sqdist(P.get(0), P.get(1))); // set the distance
-        //calculate minDistance from the firt k+z+1 point
-        for(int i = 0; i <(k+z+1); i++){
-            for (int j = 0; j<(k+z+1); j++){
-                if(true) {
-                    double dist = Math.sqrt(Vectors.sqdist(P.get(i),P.get(j)));
-                    if(dist<minDist){
-                        minDist = dist;
-                    }
-                }
-            }
-        }
-        double r = (minDist)/2;
-
-        while(true){
-            ArrayList<Vector> Z = P;
-            ArrayList<Vector> S = new ArrayList<>();
-            double Wz = 0; // Calculate the sum of all weight
-            for (double w: W) {
-                Wz = Wz + w;
-            }
-
-            while(( S.size() < k )&&( Wz > 0)){
-                double max = 0;
-                Vector newCenter = null;
-                for( int i = 0; i<P.size(); i++){
-
-                    ArrayList<Vector> ball = new ArrayList<>();
-                    double ballWeight = 0;
-                    for( int j = 0; j< P.size(); j++){
-                        if(i!=j){
-                            double dist = Math.sqrt(Vectors.sqdist(P.get(i), P.get(j)));
-                            if(dist<(1+2*alpha)*r){
-                                ball.add(P.get(j));
-                                ballWeight += W.get(j);
-                            }
-                        }
-
-                    }
-
-                    if(ballWeight > max){
-                        max = ballWeight;
-                        newCenter = P.get(i);
-                    }
-                }
-
-
-                if(newCenter != null) {
-                    S.add(newCenter);
-                    ArrayList<Vector> Bz = new ArrayList<>();
-                    for (int j = 0; j < P.size(); j++) {
-
-                        double dist = Math.sqrt(Vectors.sqdist(P.get(j), newCenter));
-                        if (dist < (3 + 4 * alpha) * r) {
-                            Z.remove(P.get(j));
-                            Wz -= W.get(j);
-                        }
-                    }
-                }
-
-            }
-            if(Wz < z){
-                System.out.println(S.toArray());
-                return S;
-            }else {
-                r = 2*r;
-            }
-
-        }
-
-
-
-
-/*
+    public static ArrayList<Vector> DummySeqWeightedOutliers(ArrayList<Vector> P, ArrayList<Long> W, int k, int z, int alpha) {
 
         System.out.println(P);
-        //todo
         //dummy example
         //dummy fixed solution just for testing the objective function in main:
         ArrayList<Vector> dummySolution = new ArrayList<>();
@@ -145,7 +69,125 @@ public class G074HW2 {
         dummySolution.add(dummyCenter);
         return dummySolution;
 
-        */
+    }
+
+    /**
+     * This method implements the weighted variant of KcenterOUT algorithm
+     * @param P the set of points
+     * @param W the set of weights
+     * @param k the number of centers
+     * @param z the number of outliers
+     * @param alpha coefficient used by the algorithm
+     * @return a solution of the K-center with z outliers problem
+     */
+    public static ArrayList<Vector> SeqWeightedOutliers(ArrayList<Vector> P, ArrayList<Long> W, int k, int z, int alpha) {
+
+
+        ArrayList<Vector> Z = new ArrayList<>();
+        double max = 0, dist, r=-1, Wz, ballWeight;
+        Vector newCenter, y;
+        ArrayList<Vector> Bz = new ArrayList<>();
+
+
+        System.out.println("Calculating r as the min distance between the first k+z+1="+(k+z+1)+" points");
+        double minDist = Math.sqrt(Vectors.sqdist(P.get(0), P.get(1))); // set the distance
+        //calculate minDistance from the firt k+z+1 point
+        for(int i = 0; i <(k+z+1); i++){
+            for (int j = 0; j<(k+z+1); j++){
+                if(i!=j){ //they must not be the same point
+                    dist = Math.sqrt(Vectors.sqdist(P.get(i),P.get(j)));
+                    //System.out.println("Distance between " + P.get(i) + " and " + P.get(j) + " is " + dist);
+                    if(dist<minDist){
+                        minDist = dist;
+                    }
+                }
+            }
+        }
+        r = (minDist)/2;
+        System.out.println("Calculated r = " + r);
+
+
+
+        while(true) {
+            System.out.println("outer while");
+            for (Vector originalPoint : P) { //deep copy of P into Z (notice that the reference of each originalPoint is copied)
+                System.out.println("copying " + originalPoint + "from P into Z");
+                Z.add(originalPoint);
+            }
+            ArrayList<Vector> S = new ArrayList<>();
+            Wz = 0; // Calculate the sum of all the weight vector's weights
+            for (double w : W) {
+                System.out.println("summing to Wz the weight: " + w);
+                Wz = Wz + w;
+            }
+            System.out.println("\nFinal Wz = " + Wz);
+
+            while ((S.size() < k) && (Wz > 0)) {
+                System.out.println("inner while [|S|=" + S.size() + ", Wz=" + Wz + "]");
+                max = 0;
+                newCenter = null;
+
+                //now we have to compute the sum of all the weights of the points which are in the ball Bz
+
+                //new code:
+
+                for (Vector x : P) { //for each point x in P (ball center)
+                    System.out.println("Considering point of P x: " + x);
+                    ballWeight = 0;
+                    //calculate ballWeight by summing all weights of the points in the ball Bz with center x and radius (1+2a)r.
+                    //a point y nof the set Z is considered in the ball Bz if his distance from the ball center x is less or equal than (1+2a)r
+                    for (int i = 0; i < Z.size(); i++) { //for each point y in Z
+                        y = Z.get(i);
+                        System.out.println("considering point of Z y: " + y);
+                        dist = Vectors.sqdist(x, y);
+                        System.out.println("calculated distance of x to y " + dist);
+
+                        if (dist <= (1 + 2 * alpha) * r) { //check if the point y is in the ball Bz(x,(1+2a)r)
+                            Bz.add(y); //add y to the ball
+                            System.out.println("The point y is in the ball. Adding its weight (" + W.get(i) + ")to the partial ballWeight sum");
+                            ballWeight += W.get(i); //sum up the weight of y
+                        }
+                    }
+                    System.out.println("\n ballWeight: " + ballWeight);
+
+                    if (ballWeight > max) { //if i found a bigger ballWaight than the max found until now
+                        max = ballWeight; //update max with current ballWeight
+                        newCenter = x; //update newCenter with current x
+                    }
+                }//end of for each point x in P
+
+                if (newCenter != null) {
+                    S.add(newCenter); //add the newCenter found into the solution set S
+                    System.out.println("*Adding newCenter (" + newCenter + ") to S\nCreating the ball Bz with center newCenter and radius (3+4a)r");
+
+                    //find a new ball Bz with center "newCenter" and radius (3+4a)r.
+                    //a point y of the set Z is considered in the ball Bz if his distance from the ball center "newCenter" is less or equal than (3+4a)r
+                    for (int i = 0; i < Z.size(); i++) { //for each point y in Z
+                        y = Z.get(i);
+                        System.out.println("considering point of Z y: " + y);
+                        dist = Vectors.sqdist(newCenter, y);
+                        System.out.println("calculated distance of newCenter to y " + dist);
+
+                        if (dist <= (3 + 4 * alpha) * r) { //check if the point y is in the ball Bz(newCenter,(1+2a)r)
+                            System.out.println("The point y is in the ball Bz(newCenter,(3+4a)r)");
+                            //remove y from Z
+                            Z.remove(i);
+                            Wz -= W.get(i);
+                        }
+                    }//end of for each point y in Z
+
+                    if (Wz <= z) {
+                        System.out.println("end of the algorithm, returning: " + S);
+                        return S;
+                    } else {
+                        r = 2 * r;
+                    }
+
+                }//end of if(newCenter!=null)
+            }//end of inner while
+        }//end of outer while
+
+        
     }
 
     /**
@@ -213,21 +255,23 @@ public class G074HW2 {
         }
 
         long startTime = System.currentTimeMillis();
+        //ArrayList<Vector> solution = DummySeqWeightedOutliers(inputPoints,weights,k,z,0);
         ArrayList<Vector> solution = SeqWeightedOutliers(inputPoints,weights,k,z,0);
 
-        long execTime = System.currentTimeMillis() - startTime;
 
-        double objective = ComputeObjective(inputPoints,solution,z);
+        long execTime = System.currentTimeMillis() - startTime;
+        Double objective = null;
+        //objective = ComputeObjective(inputPoints,solution,z);
 
         //Output printing
         System.out.println("Input size n = " + inputPoints.size());
         System.out.println("Number of centers k = " + k);
         System.out.println("Number of outliers z = " + z);
-        System.out.println("Initial guess = ");
+        /*System.out.println("Initial guess = ");
         System.out.println("Final guess = ");
         System.out.println("Number of guesses = ");
         System.out.println("Objective function = " + objective);
-        System.out.println("Time of SeqWeightedOutliers = " + execTime);
+        System.out.println("Time of SeqWeightedOutliers = " + execTime);*/
 
     }
 }
